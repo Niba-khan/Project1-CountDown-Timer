7701a7a5c26dc83d63e844eb3ff0487dcbc3d382
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2024-10-15 18:32:16 +0500

    Project1

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import CountDownTimer from "@/components/countdowntimer";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <CountDownTimer/>
+    </>
+  )
+}
